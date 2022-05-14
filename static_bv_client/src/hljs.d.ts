279cbb8826c4e0ce59b52a70b0b8e8c23798c2d3
adam-lafontaine <adam.lafontaine@gmail.com>
2022-05-14 09:50:59 -0400

    update hljs.d.ts

--- static_bv_client/src/hljs.d.ts
+++ static_bv_client/src/hljs.d.ts
@@ -1,2 +1,4 @@
 declare module 'highlight.js/lib/core';
 declare module 'highlight.js/lib/languages/cpp';
+declare module 'highlight.js/lib/languages/makefile';
+declare module 'highlight.js/lib/languages/plaintext';
